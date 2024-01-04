fc5a6bfcd612ecd607bcd9b15344727ad650c7da
JPumpkin1223 <jpark9595@naver.com>
2024-01-04 15:53:58 +0900

    Add CircularQueue implementation
    This commit adds the implementation of the CircularQueue class. The CircularQueue class represents a circular queue data structure with enqueue, dequeue, isFull, isEmpty, getSize, and display methods. It also includes additional functionality to handle overwriting elements when the queue is full.

---
+++ circular_queue.py
@@ -0,0 +1,56 @@
+class CircularQueue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.head = self.tail = self.size = 0
+        self.mac_set = set()
+        self.new_mac_set = set()
+        self.disappeared_mac_set = set()
+        self.last_updated = None
+
+    def enqueue(self, item: list):
+        if self.isFull():
+            self.enqueueWithOverwrite(item)
+            return True
+        self.queue[self.tail] = item[1]
+        self.tail = (self.tail + 1) % self.capacity
+        self.size += 1
+        self.new_mac_set = set(item[1]) - self.mac_set
+        self.disappeared_mac_set = self.mac_set - set(item[1])
+        self.last_updated = item[0]
+        self.mac_set.update(set(item[1]))
+        return True
+    
+    def enqueueWithOverwrite(self, item):
+        if self.isFull():
+            head_mac_c = set(self.queue[self.head]) - self.mac_set
+            self.mac_set.difference_update(head_mac_c)
+
+            # Move the head to the next position to overwrite the oldest element
+            self.head = (self.head + 1) % self.capacity
+            self.size -= 1
+
+        # Proceed with the normal enqueue process
+        return self.enqueue(item)
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("Queue is empty")
+            return None
+        item = self.queue[self.head]
+        self.queue[self.head] = None
+        self.head = (self.head + 1) % self.capacity
+        self.size -= 1
+        return item
+
+    def isFull(self):
+        return self.size == self.capacity
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def getSize(self):
+        return self.size
+    
+    def display(self):
+        print(self.queue)
